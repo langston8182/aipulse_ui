22914b7e82e068d0fd33c65e77b218382e101e39
Cyril Marchive <cyril.marchive@gmail.com>
2025-02-18 19:11:19 +0100

    center text during loading

--- src/App.tsx
+++ src/App.tsx
@@ -110,7 +110,7 @@ function App() {
   if (loading) {
     return (
         <div className="min-h-screen bg-gray-50 flex items-center justify-center">
-          <div className="text-center">
+          <div className="flex flex-col items-center">
             <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-indigo-600"></div>
             <p className="mt-4 text-gray-600">Chargement des articles...</p>
           </div>
