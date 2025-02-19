c54b1fe89d6907517b0446fbc4be20cf61c92880
Cyril Marchive <cyril.marchive@gmail.com>
2025-02-19 01:18:18 +0100

    Bug fix newsletter

--- src/services/newsletter.ts
+++ src/services/newsletter.ts
@@ -1,6 +1,7 @@
 import type { EmailRequest } from '../types';
 import { generateToken } from '../utils/crypto';
 import { sendEmail } from './email';
+import config from '../config.json';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
